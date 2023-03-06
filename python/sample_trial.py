1d5579e22d27ce355296bbd705aaa6305a08f32d
hirota-yu <hrtyuki0618@outlook.jp>
2023-03-06 16:04:14 +0900

    残り牌のカウントの実装

--- python/sample_trial.py
+++ python/sample_trial.py
@@ -15,6 +15,8 @@ from mjx.const import ActionType
 from mjx.env import MjxEnv
 from mjx.observation import Observation
 from mjx.visualizer.selector import Selector
+from mjx.const import EventType, PlayerIdx, TileType
+from mjx.event import Event
 
 from server import convert_log
 from client.agent import CustomAgentBase
@@ -27,18 +29,113 @@ from client.agent import CustomAgentBase
 class MyAgent(CustomAgentBase):
     def __init__(self):
         super().__init__()
+        self.remining_tiles = [[4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],[1,1,1]]
+        self.action_mode = "menzen"
 
     def act(self, obs: mjx.Observation) -> mjx.Action:
         hand = obs.MjxLargeV0().current_hand(obs)
         target = obs.MjxLargeV0().target_tile(obs)
         riichi = obs.MjxLargeV0().under_riichis(obs)
+        discarded_tiles = obs.MjxLargeV0().discarded_tiles(obs)
+        discarded_from_fand = obs.MjxLargeV0().discarded_from_hand(obs)
+        ignored_tiles = obs.MjxLargeV0().ignored_tiles(obs)
+        doras = obs.doras()
         
-        print(riichi)
+
+        if not (1 in discarded_tiles[0] or 1 in discarded_tiles[3] or 1 in discarded_tiles[6] or 1 in discarded_tiles[9]):
+            self.remining_tiles = [[4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],[1,1,1]]
+        
+
+        self.remining_tiles = [[4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],[1,1,1]]
+        for dora in doras:
+            opened_dora = 0
+            if not dora in [0,9,18,27,31]:
+                opened_dora = dora-1
+            elif dora==9:
+                opened_dora = 17
+            elif dora==18:
+                opened_dora = 26
+            elif dora==27:
+                opened_dora = 31
+            elif dora==31:
+                opened_dora = 33   
+            self.remining_tiles[0][opened_dora] -= 1
+
+        check_red_dora = [0,0,0]
+        if hand[4][0]==1:
+            check_red_dora[0]=1
+        elif hand[5][0]==1:
+            check_red_dora[1]=1
+        elif hand[6][0]==1:
+            check_red_dora[2]=1
+        self.remining_tiles = [self.remining_tiles[0], [x-y for (x,y) in zip(self.remining_tiles[1],check_red_dora)]]
+        for i in range(4):
+            self.remining_tiles = [[x-y for (x,y) in zip(self.remining_tiles[0],hand[i])],self.remining_tiles[1]]
+        for e in obs.events():
+                if e.type() == EventType.DISCARD or e.type() == EventType.TSUMOGIRI:
+                    self.remining_tiles[0][e.tile().type()] -= 1
+                    if e.tile().is_red() and e.tile().type() == 4:
+                        self.remining_tiles[1][0]=0
+                    elif e.tile().is_red() and e.tile().type() == 13:
+                        self.remining_tiles[1][1]=0
+                    elif e.tile().is_red() and e.tile().type() == 22:
+                        self.remining_tiles[1][2]=0
+
+                elif e.type() in [
+                    EventType.CHI,
+                    EventType.PON,
+                    EventType.OPEN_KAN,
+                ]:
+                    for t in e.open().tiles():
+                        self.remining_tiles[0][t.type()] -= 1
+                        if t.is_red() and t.type() == 4:
+                            self.remining_tiles[1][0]=0
+                        elif t.is_red() and t.type() == 13:
+                            self.remining_tiles[1][1]=0
+                        elif t.is_red() and t.type() == 22:
+                            self.remining_tiles[1][2]=0
+                    self.remining_tiles[0][e.open().last_tile().type()] += 1
+                
+                elif e.type() in [
+                    EventType.CLOSED_KAN,
+                ]:
+                    for t in e.open().tiles():
+                        self.remining_tiles[0][t.type()] -= 1
+                        if t.is_red() and t.type() == 4:
+                            self.remining_tiles[1][0]=0
+                        elif t.is_red() and t.type() == 13:
+                            self.remining_tiles[1][1]=0
+                        elif t.is_red() and t.type() == 22:
+                            self.remining_tiles[1][2]=0
+
+                
+                elif e.type() == EventType.ADDED_KAN:
+                    t = e.open().last_tile()
+                    self.remining_tiles[0][t.type()] -= 1
+                    if t.is_red() and t.type() == 4:
+                        self.remining_tiles[1][0]=0
+                    elif t.is_red() and t.type() == 13:
+                        self.remining_tiles[1][1]=0
+                    elif t.is_red() and t.type() == 22:
+                        self.remining_tiles[1][2]=0
         
 
         legal_actions = obs.legal_actions()
+        kyusyu_actions = [a for a in legal_actions if a.type() == ActionType.ABORTIVE_DRAW_NINE_TERMINALS]
+        if len(kyusyu_actions) >= 1:
+            assert len(kyusyu_actions) == 1
+            self.action_mode = "kyusyu"
+
         if len(legal_actions) == 1:
             return legal_actions[0]
+
+        if self.action_mode == "kyusyu":
+            legal_discards = [
+            a for a in legal_actions if a.type() in [ActionType.DISCARD, ActionType.TSUMOGIRI]
+            ]
+            for a in legal_discards:
+                print(a.tile().type())
+
 
         # if it can win, just win
         win_actions = [a for a in legal_actions if a.type() in [ActionType.TSUMO, ActionType.RON]]
@@ -68,8 +165,8 @@ class MyAgent(CustomAgentBase):
             a for a in legal_actions if a.type() in [ActionType.ADDED_KAN] 
         ]
         if len(added_kan_actions) >= 1:
-            pass_action = [a for a in legal_actions if a.type() == ActionType.PASS][0]
-            return pass_action
+            assert len(added_kan_actions) == 1
+            return added_kan_actions[0]
 
         closed_kan_actions = [
             a for a in legal_actions if a.type() in [ActionType.CLOSED_KAN]
@@ -86,6 +183,7 @@ class MyAgent(CustomAgentBase):
         effective_discards = [
             a for a in legal_discards if a.tile().type() in effective_discard_types
         ]
+
         if len(effective_discards) > 0:
             return random.choice(effective_discards)
 
