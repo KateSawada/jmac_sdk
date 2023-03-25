eb3b369d8aa924470bca3e87699c1fa0dbd91e2b
hirakuuuu <m.hiraku1129@gmail.com>
2023-03-25 20:08:53 +0900

    [add] サーバーへの接続時に，クライアントが自分の名前を送信する

--- python/client/client.py
+++ python/client/client.py
@@ -64,7 +64,7 @@ class SocketIOClient:
         self.Namespace.on_server_to_client = self.on_server_to_client
         self.Namespace.on_ask_act = self.on_ask_act
     # 初期化
-    def __init__(self,ip,port,namespace,query,agent,room_id):
+    def __init__(self,ip,port,namespace,query,agent,room_id,player_name):
         self.ip_         = ip
         self.port_       = port
         self.namespace_  = namespace
@@ -76,6 +76,7 @@ class SocketIOClient:
         self.sio_.register_namespace(self.Namespace)
         self.agent = agent
         self.room_id = room_id
+        self.player_name = player_name
     # 接続確認
     def isConnect(self):
         return self.is_connect_
--- python/sample_client.py
+++ python/sample_client.py
@@ -35,6 +35,7 @@ if __name__ == "__main__":
         query='secret',
         agent=my_agent,  # プレイヤーの指定
         room_id=123,  # 部屋のID．4人で対局させる時は，同じIDを指定する．
+        player_name='hogehoge' # プレイヤーの名前
         )
     # SocketIO Client インスタンスを実行
     sio_client.run()
